6b0dfbe46f8fc23048555c6edc21d3c0c2b694ba
Ioram Gordadze <Yoh0xFF@gmail.com>
2021-10-14 18:24:09 +0400

    Reformat codebase.

--- knexfile.js
+++ knexfile.js
@@ -2,19 +2,19 @@ const os = require('os');
 const dir = os.homedir();
 
 module.exports = {
-    development: {
-        client: 'sqlite3',
+  development: {
+    client: 'sqlite3',
 
-        connection: {
-            filename: `${ dir }/graphql.sqlite`
-        },
+    connection: {
+      filename: `${ dir }/graphql.sqlite`
+    },
 
-        pool: {
-            afterCreate: (conn, cb) => {
-                conn.run('PRAGMA foreign_keys = ON;', cb);
-            }
-        },
+    pool: {
+      afterCreate: (conn, cb) => {
+        conn.run('PRAGMA foreign_keys = ON;', cb);
+      }
+    },
 
-        useNullAsDefault: true
-    }
+    useNullAsDefault: true
+  }
 };
